a114a26152029cd1225aa14cd2f14413a46b3194
Konrad 'ktoso' Malawski <ktoso@apple.com>
2025-05-05 13:08:08 +0900

    Showcase returning java object created in Swift JNI call
    We must reinvent how we promote values to global refs etc. Without the
    promotion we get a null. We need to manage the retains automatically as
    we return from a downcall in any wrapped java wrapper class etc...

--- Sources/JavaKit/JavaObject+MethodCalls.swift
+++ Sources/JavaKit/JavaObject+MethodCalls.swift
@@ -187,6 +187,7 @@ extension AnyJavaObject {
       parameterTypes: repeat (each Param).self,
       resultType: Result.self
     )
+    print("[swift][jni] Call '\(methodName)' for result \(Result.self)")
     return try javaMethodCall(
       method: methodID,
       args: repeat each arguments
